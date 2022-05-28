d4513ca21f63d576124119ef6eb5af409a5e5892
java <java@java-PC>
2022-05-28 13:18:36 +0530

    test ecms project

---
+++ test_ecms/src/test_ecms/TestEcms.java
@@ -0,0 +1,11 @@
+package test_ecms;
+
+public class TestEcms {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Java World!!!");
+	}
+
+}
